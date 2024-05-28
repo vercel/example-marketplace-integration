b0f71c3e7b300d8009c054193a90e4f582db0e72
Adrian Cooney <adrian.cooney@vercel.com>
2024-05-28 16:43:12 +0100

    Disable scary account suspended banner

--- app/v1/installations/[installationId]/resources/route.ts
+++ app/v1/installations/[installationId]/resources/route.ts
@@ -1,35 +1,13 @@
 import { listResources, provisionResource } from "@/lib/partner";
 import { readRequestBodyWithSchema } from "@/lib/utils";
 import { withAuth } from "@/lib/vercel/auth";
-import { Resource, provisionResourceRequestSchema } from "@/lib/vercel/schemas";
+import { provisionResourceRequestSchema } from "@/lib/vercel/schemas";
 
 export const dynamic = "force-dynamic";
-
-const notificationsMock: Resource["notification"][] = [
-  {
-    title: "Account is suspended",
-    message:
-      "Your account has been suspended due to a billing issue. Please update your payment information to resume service.",
-    href: "https://vercel.com/account/billing",
-    level: "error",
-  },
-  {
-    title: "Initializing",
-    message:
-      "Your resource is currently initializing. This may take a few minutes.",
-    level: "warn",
-  },
-];
 
 export const GET = withAuth(async (claims, request) => {
   const ids = request.nextUrl.searchParams.getAll("ids");
   const resources = await listResources(claims.installation_id, ids);
-  if (resources.resources[0]) {
-    resources.resources[0].notification = notificationsMock[0];
-  }
-  if (resources.resources[1]) {
-    resources.resources[1].notification = notificationsMock[1];
-  }
   return Response.json(resources);
 });
 
