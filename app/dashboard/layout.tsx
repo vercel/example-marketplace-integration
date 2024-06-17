9a72e23e79399bff77c62e79828f4b1c08b624fb
Adrian Cooney <adrian.cooney@vercel.com>
2024-06-17 16:38:11 +0100

    Add user_name and user_avatar_url to the JWT

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,38 +8,50 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const session = await getSession();
-
-  const [account] = await Promise.all([
-    getAccountInfo(session.installation_id),
-  ]);
+  const account = await getAccountInfo(session.installation_id);
 
   return (
     <div className="w-[800px] mx-auto">
       <header className="bg-blue-100 p-4">
-        <div className="flex flex-row gap-4 items-center">
-          <div className="flex flex-row items-center">
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/logo.png" alt="Logo" className="h-6" />
-            <svg
-              className="h-6"
-              data-testid="geist-icon"
-              fill="none"
-              height="24"
-              shape-rendering="geometricPrecision"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="1.5"
-              viewBox="0 0 24 24"
-              width="24"
-              style={{ color: "", width: 22, height: 22 }}
-            >
-              <path d="M16.88 3.549L7.12 20.451"></path>
-            </svg>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src="/vlogo.svg" alt="Logo" className="h-5" />
+        <div className="flex justify-between items-center">
+          <div className="flex flex-row gap-4 items-center">
+            <div className="flex flex-row items-center">
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/logo.png" alt="Logo" className="h-6" />
+              <svg
+                className="h-6"
+                data-testid="geist-icon"
+                fill="none"
+                height="24"
+                shape-rendering="geometricPrecision"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+                viewBox="0 0 24 24"
+                width="24"
+                style={{ color: "", width: 22, height: 22 }}
+              >
+                <path d="M16.88 3.549L7.12 20.451"></path>
+              </svg>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src="/vlogo.svg" alt="Logo" className="h-5" />
+            </div>
+            <h1 className="text-xl font-bold">
+              {`${account.name}'s`} Dashboard
+            </h1>
           </div>
-          <h1 className="text-xl font-bold">{`${account.name}'s`} Dashboard</h1>
+          <div className="flex items-center gap-2">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <span>{session.user_name || "Unknown"}</span>
+            <img
+              alt="Avatar"
+              src={
+                session.user_avatar_url || "https://vercel.com/api/www/avatar"
+              }
+              className="w-7 h-7 rounded-full"
+            />
+          </div>
         </div>
         <nav className="mt-4">
           <Nav />
