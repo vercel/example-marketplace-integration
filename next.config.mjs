8408feaa9dbc08ce48aa9f410c42ae640cbf6f57
dvoytenko <dima.voytenko@vercel.com>
2025-03-18 15:16:39 -0700

    Try COOP unsafe-none

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
       headers: [
         {
           key: "Cross-Origin-Opener-Policy",
-          value: "same-origin-allow-popups",
+          value: "unsafe-none",
         },
       ],
     },
