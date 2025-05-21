edb6a8bac5135716c7e435e8e18a59b1fbdda68e
dvoytenko <dima.voytenko@vercel.com>
2025-05-21 06:42:56 -0700

    Experiment with Cross-Origin-Opener-Policy=same-origin

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
       headers: [
         {
           key: "Cross-Origin-Opener-Policy",
-          value: "unsafe-none",
+          value: "same-origin",
         },
       ],
     },
