dfd70cf941d7de9275767d0f4afaa7eb845d11c8
Hayden Bleasel <hello@haydenbleasel.com>
2026-01-01 14:15:02 -0800

    Update Configure page

--- app/connect/configure/page.tsx
+++ app/connect/configure/page.tsx
@@ -1,12 +1,22 @@
-export default async function Page({
-  searchParams: { configurationId },
-}: {
-  searchParams: { configurationId: string };
-}) {
+import { notFound } from "next/navigation";
+
+/**
+ * Configure page for the connect integration.
+ * This page is used when the user presses "Configure" on the Vercel integration page.
+ */
+const Page = async (props: PageProps<"/connect/configure">) => {
+  const { configurationId } = await props.searchParams;
+
+  if (typeof configurationId !== "string") {
+    return notFound();
+  }
+
   return (
     <div className="space-y-10 p-10 text-center">
       <h1 className="font-medium text-lg">Nothing to configure here. 👀</h1>
       <h3 className="font-mono">{configurationId}</h3>
     </div>
   );
-}
+};
+
+export default Page;
